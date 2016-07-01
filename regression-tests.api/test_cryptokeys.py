06194f632f1acec3f4675abef429c51250559988
MrMonkey <christoph.graebnitz@gmail.com>
2016-07-01 13:34:36 +0200

    Reworked tests in favor of travis test envrionment.

--- regression-tests.api/test_cryptokeys.py
+++ regression-tests.api/test_cryptokeys.py
@@ -5,54 +5,37 @@ import sys
 from test_helper import ApiTestCase
 
 class Cryptokeys(ApiTestCase):
-    zone = "cryptokeyzone"
+    zone = "cryptokeyzone.com."
     def setUp(self):
         super(Cryptokeys, self).setUp()
-        try:
-            subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "create-zone", self.zone])
-        except subprocess.CalledProcessError as e:
-            self.fail("Couldn't create zone: "+e.output)
-
+        payload = {
+            'name': self.zone,
+            'kind': 'Native',
+            'nameservers': ['ns1.example.com.', 'ns2.example.com.']
+        }
+        # Create zone for testing
+        r = self.session.post(
+            self.url("/api/v1/servers/localhost/zones"),
+            data=json.dumps(payload),
+            headers={'content-type': 'application/json'})
+        self.assert_success_json(r)
 
     def tearDown(self):
         super(Cryptokeys,self).tearDown()
-        try:
-            subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "delete-zone", self.zone])
-        except subprocess.CalledProcessError as e:
-            self.fail("Couldn't delete zone: "+e.output)
+        # Removes zone
+        r = self.session.delete(self.url("/api/v1/servers/localhost/zones/" + self.zone))
+        self.assertEquals(r.status_code, 204)
+        self.assertNotIn('Content-Type', r.headers)
 
-
-    # This methode test the DELETE api call. Sometimes 200 or 422 are valid return codes,
-    # because it "could" happen that the backend crashes during the test or the remove function returns false.
-    # In this case 422 is correct. I don't know how to force false backend behavior. So i assume that the backend
-    # works correct.
-    def test_delete(self):
-        try:
-            keyid = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "add-zone-key", self.zone, "ksk"])
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil add-zone-key failed: "+e.output)
-
-        #checks the status code. I don't know how to test explicit that the backend fail removing a key.
-        r = self.session.delete(self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+keyid))
-        self.assertTrue(r.status_code == 200 or r.status_code == 422)
-
-        # Check that the key is actually deleted
-        try:
-            out = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "list-keys", self.zone])
-            self.assertFalse(self.zone in out)
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil list-keys failed: " + e.output)
-
-        #checks for not covered zonename
-        r = self.session.delete(self.url("/api/v1/servers/localhost/zones/"+self.zone+"fail/cryptokeys/"+keyid))
-        self.assertEquals(r.status_code, 400)
-
-        #checks for key is gone. Its ok even if no key had to be deleted. Or something went wrong with the backend.
-        r = self.session.delete(self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+keyid))
-        self.assertTrue(r.status_code == 200 or r.status_code == 422)
+    # Retuns all public data from all keys from self.zone
+    def get_keys(self):
+        r = self.session.get(
+            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys"))
+        self.assert_success_json(r)
+        return r.json()
 
     # Prepares the json object for Post and sends it to the server
-    def add_key(self, content='', type='ksk', active='true' , algo='', bits=0):
+    def add_key(self, content='', type='ksk', active='true' , algo='', bits=0, assertion=True):
         if algo == '':
             payload = {
                 'keytype': type,
@@ -72,33 +55,58 @@ class Cryptokeys(ApiTestCase):
             self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys"),
             data=json.dumps(payload),
             headers={'content-type': 'application/json'})
+        if assertion:
+            self.assert_success_json(r)
+            self.assertEquals(r.status_code, 201)
         return r
 
+    # This methode test the DELETE api call. Sometimes 200 or 422 are valid return codes,
+    # because it "could" happen that the backend crashes during the test or the remove function returns false.
+    # In this case 422 is correct. I don't know how to force false backend behavior. So i assume that the backend
+    # works correct.
+    def test_delete(self):
+        r = self.add_key(type='ksk')
+
+        keyid = str(r.json()['id'])
+
+        #checks the status code. I don't know how to test explicit that the backend fail removing a key.
+        r = self.session.delete(self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+keyid))
+        self.assertTrue(r.status_code == 200 or r.status_code == 422)
+
+        # Check that the key is actually deleted
+        keys = [k['id'] for k in self.get_keys()]
+        self.assertFalse(keyid in keys)
+
+        #checks for not covered zonename
+        r = self.session.delete(self.url("/api/v1/servers/localhost/zones/"+self.zone+"fail/cryptokeys/"+keyid))
+        self.assertEquals(r.status_code, 400)
+
+        #checks for key is gone. Its ok even if no key had to be deleted. Or something went wrong with the backend.
+        r = self.session.delete(self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+keyid))
+        self.assertTrue(r.status_code == 200 or r.status_code == 422)
+
     # Removes a key by id using the pdnsutil command
-    def remove_key(self, key_id):
-        try:
-            subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "remove-zone-key", self.zone, str(key_id)])
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil remove-zone-key failed: "+e.output)
+    def remove_key(self, keyid):
+        #checks for key is gone. Its ok even if no key had to be deleted. Or something went wrong with the backend.
+        r = self.session.delete(self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+str(keyid)))
+        self.assertTrue(r.status_code == 200)
 
     # Tests POST for a positiv result and deletes the added key
     def post_helper(self,content='', algo='', bits=0):
         r = self.add_key(content=content, algo=algo, bits=bits)
-        self.assert_success_json(r)
-        self.assertEquals(r.status_code, 201)
+
         response = r.json()
+
         # Only a ksk added, so expected type is csk
         self.assertEquals(response['keytype'], 'csk')
-        key_id = response['id']
+        keyid = response['id']
+
         # Check if the key is actually added
-        try:
-            out = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "list-keys", self.zone])
-            self.assertTrue(self.zone in out)
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil list-keys failed: " + e.output)
+        keys = [k['id'] for k in self.get_keys()]
+        self.assertTrue(keyid in keys)
 
         # Remove it for further testing
-        self.remove_key(key_id)
+        self.remove_key(keyid)
 
     def test_post(self):
         # Test add a key with default algorithm
@@ -126,143 +134,120 @@ class Cryptokeys(ApiTestCase):
                             "Coefficient: eVrHe/kauqOewSKndIImrg==)\n")
 
         # Test wrong key format
-        r = self.add_key(content="trollololoooolll")
+        r = self.add_key(content="trollololoooolll",assertion=False)
         self.assert_error_json(r)
         self.assertEquals(r.status_code, 422)
         self.assertIn("Wrong key format!",r.json()['error'])
 
         # Test wrong keytype
-        r = self.add_key(type='sdfdhhgj')
+        r = self.add_key(type='sdfdhhgj',assertion=False)
         self.assert_error_json(r)
         self.assertEquals(r.status_code, 422)
         self.assertIn("Invalid keytype",r.json()['error'])
 
         #Test unsupported algorithem
-        r = self.add_key(algo='lkjhgf')
+        r = self.add_key(algo='lkjhgf',assertion=False)
         self.assert_error_json(r)
         self.assertEquals(r.status_code, 422)
         self.assertIn("Unknown algorithm:",r.json()['error'])
 
         #Test add a key and forgot bits
-        r = self.add_key(algo="rsasha256")
+        r = self.add_key(algo="rsasha256",assertion=False)
         self.assert_error_json(r)
         self.assertEquals(r.status_code, 422)
         self.assertIn("key requires the size (in bits) to be passed", r.json()['error'])
 
         #Test wrong bit size
-        r = self.add_key(algo=10, bits=30)
+        r = self.add_key(algo=10, bits=30,assertion=False)
         self.assert_error_json(r)
         self.assertEquals(r.status_code,422)
         self.assertIn("Wrong bit size!", r.json()['error'])
 
         #Test can't guess key size
-        r = self.add_key(algo=15)
+        r = self.add_key(algo=15,assertion=False)
         self.assert_error_json(r)
         self.assertEquals(r.status_code,422)
         self.assertIn("Can't guess key size for algorithm", r.json()['error'])
 
-    def test_put_activate_deactivate_key(self):
+    def is_key_active(self, key_id):
+        # check if key is activated
+        for k in self.get_keys():
+            if k['id'] == key_id:
+                return k['active']
 
+    def test_put_activate_deactivate_key(self):
         #create key
-        try:
-            keyid = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "add-zone-key", self.zone, "ksk"])
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil add-zone-key failed: "+e.output)
+        r = self.add_key()
+        keyid = r.json()['id']
 
         # activate key
         payload = {
             'active': True
         }
         o = self.session.put(
-            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+keyid),
+            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+str(keyid)),
             data=json.dumps(payload),
             headers={'content-type': 'application/json'})
         self.assertEquals(o.status_code, 200)
 
         # check if key is activated
-        try:
-            out = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "show-zone", self.zone])
-            self.assertTrue("Active" in out)
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil show-zone failed: " + e.output)
+        self.assertTrue(self.is_key_active(keyid))
 
         # deactivate key
         payload2 = {
             'active': False
         }
         q = self.session.put(
-            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+keyid),
+            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+str(keyid)),
             data=json.dumps(payload2),
             headers={'content-type': 'application/json'})
         self.assertEquals(q.status_code, 200)
         self.assert_success_json(q)
 
         # check if key is deactivated
-        try:
-            out = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "show-zone", self.zone])
-            self.assertTrue("Inactive" in out)
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil show-zone failed: " + e.output)
+        self.assertFalse(self.is_key_active(keyid))
 
         # Remove it for further testing
-        try:
-            subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "remove-zone-key", self.zone, str(keyid)])
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil remove-zone-key failed: "+e.output)
+        self.remove_key(keyid)
 
     def test_put_deactivate_deactivated_activate_activated_key(self):
-
         #create key
-        try:
-            keyid = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "add-zone-key", self.zone, "ksk"])
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil add-zone-key failed: "+e.output)
+        r = self.add_key()
+        keyid = r.json()['id']
 
         # deactivate key
         payload = {
             'active': False
         }
         q = self.session.put(
-            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+keyid),
+            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+str(keyid)),
             data=json.dumps(payload),
             headers={'content-type': 'application/json'})
         self.assertEquals(q.status_code, 200)
         self.assert_success_json(q)
 
         # check if key is still deactivated
-        try:
-            out = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "show-zone", self.zone])
-            self.assertTrue("Inactive" in out)
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil show-zone failed: " + e.output)
+        self.assertFalse(self.is_key_active(keyid))
 
         # activate key
         payload2 = {
             'active': True
         }
         o = self.session.put(
-            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+keyid),
+            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+str(keyid)),
             data=json.dumps(payload2),
             headers={'content-type': 'application/json'})
         self.assertEquals(o.status_code, 200)
 
         # check if key is activated
-        try:
-            out = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "show-zone", self.zone])
-            self.assertTrue("Active" in out)
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil show-zone failed: " + e.output)
+        self.assertTrue(self.is_key_active(keyid))
 
         #activate again
         z = self.session.put(
-            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+keyid),
+            self.url("/api/v1/servers/localhost/zones/"+self.zone+"/cryptokeys/"+str(keyid)),
             data=json.dumps(payload2),
             headers={'content-type': 'application/json'})
         self.assertEquals(z.status_code, 200)
 
         # check if key is still activated
-        try:
-            out = subprocess.check_output(["../pdns/pdnsutil", "--config-dir=.", "show-zone", self.zone])
-            self.assertTrue("Active" in out)
-        except subprocess.CalledProcessError as e:
-            self.fail("pdnsutil show-zone failed: " + e.output)
+        self.assertTrue(self.is_key_active(keyid))
